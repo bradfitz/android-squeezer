7478af6569b4c06f882fe2010eb85c4b6f985b15
Brad Fitzpatrick <brad@danga.com>
2009-08-10 21:02:57 -0700

    start of reading from socket, notifications, etc.

--- src/com/danga/squeezeremote/SqueezeService.java
+++ src/com/danga/squeezeremote/SqueezeService.java
@@ -1,6 +1,8 @@
 package com.danga.squeezeremote;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -9,20 +11,36 @@ import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicReference;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
 import android.os.IBinder;
 import android.os.RemoteException;
 import android.util.Log;
 
 public class SqueezeService extends Service {
-	private final String TAG = "SqueezeService";
+	private static final String TAG = "SqueezeService";
+	private static final int PLAYBACKSERVICE_STATUS = 1;
+		
 	private final AtomicBoolean isConnected = new AtomicBoolean(false);
 	private final AtomicBoolean isPlaying = new AtomicBoolean(false);
 	private final ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
 	private final AtomicReference<Socket> socketRef = new AtomicReference<Socket>();
 	private final AtomicReference<IServiceCallback> callback = new AtomicReference<IServiceCallback>();
 	private final AtomicReference<PrintWriter> socketWriter = new AtomicReference<PrintWriter>();
+	private Thread listeningThread = null;
+	
+    @Override
+    public void onCreate() {
+    	super.onCreate();
+    	
+        // Clear leftover notification in case this service previously got killed while playing                                                
+        NotificationManager nm = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        nm.cancel(PLAYBACKSERVICE_STATUS);
+    }
 	
 	@Override
 	public IBinder onBind(Intent intent) {
@@ -50,6 +68,26 @@ public class SqueezeService extends Service {
 		setConnectionState(false);
 	}
 
+	private synchronized void sendCommand(String string) {
+		PrintWriter writer = socketWriter.get();
+		if (writer == null) return;
+		Log.v(TAG, "SENDING: " + string);
+		writer.println(string);
+	}
+	
+	private void onLineReceived(String serverLine) {
+		Log.v(TAG, "LINE: " + serverLine);
+	}
+	
+	private void startListeningThread() {
+		sendCommand("listen 1");
+		if (listeningThread != null) {
+			listeningThread.stop();
+		}
+		listeningThread = new ListeningThread(socketRef.get());
+		listeningThread.start();
+	}
+
 	private void setConnectionState(boolean currentState) {
 		isConnected.set(currentState);
 		if (callback.get() == null) {
@@ -65,6 +103,23 @@ public class SqueezeService extends Service {
 	
 	private void setPlayingState(boolean state) {
 		isPlaying.set(state);
+        NotificationManager nm =
+            (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+		if (state) {
+            Notification status = new Notification();
+            //status.contentView = views;
+            PendingIntent pIntent = PendingIntent.getActivity(this, 0,
+                    new Intent(this, SqueezeRemoteActivity.class), 0);
+            status.setLatestEventInfo(this, "Music Playing", "Content Text", pIntent);
+            status.flags |= Notification.FLAG_ONGOING_EVENT;
+            status.icon = R.drawable.stat_notify_musicplayer;
+            //status.contentIntent = PendingIntent.getActivity(this, 0,
+            //        new Intent(this, SqueezeRemoteActivity.class), 0);
+            nm.notify(PLAYBACKSERVICE_STATUS, status);
+		} else {
+	        nm.cancel(PLAYBACKSERVICE_STATUS);
+		}
+		
 		if (callback.get() == null) {
 			return;
 		}
@@ -113,6 +168,7 @@ public class SqueezeService extends Service {
 						Log.d(TAG, "writer == " + socketWriter.get());
 						setConnectionState(true);
 						Log.d(TAG, "connection state broadcasted true.");
+						startListeningThread();
 					} catch (SocketTimeoutException e) {
 						Log.e(TAG, "Socket timeout connecting to: " + hostPort);
 					} catch (IOException e) {
@@ -133,10 +189,15 @@ public class SqueezeService extends Service {
 				return false;
 			}
 			Log.v(TAG, "pause...");
-			// Assume for now (this might be wrong) that the state toggles.
-			// We'll hear from the socket soon enough what the actual state is.
-			setPlayingState(!isPlaying.get());
-			socketWriter.get().println("00%3A04%3A20%3A17%3A04%3A7f pause");
+			if (isPlaying.get()) {
+				setPlayingState(false);
+				sendCommand("00%3A04%3A20%3A17%3A04%3A7f pause 1");
+			} else {
+				setPlayingState(true);
+				// TODO: use 'pause 0 <fade_in_secs>' to fade-in if we knew it was
+				// actually paused (as opposed to not playing at all) 
+				sendCommand("00%3A04%3A20%3A17%3A04%3A7f play");
+			}
 			Log.v(TAG, "paused.");
 			return true;
 		}
@@ -147,7 +208,7 @@ public class SqueezeService extends Service {
 			}
 			Log.v(TAG, "play..");
 			isPlaying.set(true);
-			socketWriter.get().println("00%3A04%3A20%3A17%3A04%3A7f play");
+			sendCommand("00%3A04%3A20%3A17%3A04%3A7f play");
 			Log.v(TAG, "played.");
 			return true;
 		}
@@ -157,7 +218,7 @@ public class SqueezeService extends Service {
 				return false;
 			}
 			isPlaying.set(false);
-			socketWriter.get().println("00%3A04%3A20%3A17%3A04%3A7f stop");
+			sendCommand("00%3A04%3A20%3A17%3A04%3A7f stop");
 			return true;
 		}
 
@@ -165,5 +226,43 @@ public class SqueezeService extends Service {
 			return isPlaying.get();
 		}
 	};
+
+	private class ListeningThread extends Thread {
+		private final Socket socket;
+		public ListeningThread(Socket socket) {
+			this.socket = socket;	
+		}
+		
+		@Override
+		public void run() {
+			BufferedReader in;
+			try {
+				in = new BufferedReader(
+						new InputStreamReader(socket.getInputStream()),
+						128);
+			} catch (IOException e) {
+				Log.v(TAG, "IOException while reading from server: " + e);
+				SqueezeService.this.disconnect();
+				return;
+			}
+			while (true) {
+				String line;
+				try {
+					line = in.readLine();
+				} catch (IOException e) {
+					Log.v(TAG, "IOException while reading from server: " + e);
+					SqueezeService.this.disconnect();
+					return;
+				}
+				if (line == null) {
+					// Socket disconnected.
+					Log.v(TAG, "Server disconnected.");
+					SqueezeService.this.disconnect();
+					return;
+				}
+				SqueezeService.this.onLineReceived(line);
+			}
+		}
+	}
 
 }
