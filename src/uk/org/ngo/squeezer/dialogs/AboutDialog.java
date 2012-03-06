da753e6234083fa08a31488a3de75c2f644ddd6a
Nik Clayton <nik@ngo.org.uk>
2012-03-06 21:47:29 +0100

    Refactor the about dialog and create a license dialog.
    Have a simpler about dialog, and push the license in to its own dialog
    so that we throw a smaller wall of text at the user.

--- src/uk/org/ngo/squeezer/dialogs/AboutDialog.java
+++ src/uk/org/ngo/squeezer/dialogs/AboutDialog.java
@@ -1,40 +1,58 @@
+/*
+ * Copyright (c) 2012 Google Inc.  All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.org.ngo.squeezer.dialogs;
 
 import uk.org.ngo.squeezer.R;
-import uk.org.ngo.squeezer.SqueezerActivity;
 import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.text.Html;
-import android.text.method.ScrollingMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class AboutDialog extends DialogFragment {
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
-        final TextView message = (TextView) getActivity().getLayoutInflater().inflate(R.layout.about_textview, null);
+        final View view = getActivity().getLayoutInflater().inflate(R.layout.about_dialog, null);
+        final TextView titleText = (TextView) view.findViewById(R.id.about_title);
 
         PackageManager pm = getActivity().getPackageManager();
         PackageInfo info;
-        String aboutTitle;
         try {
             info = pm.getPackageInfo("uk.org.ngo.squeezer", 0);
-            aboutTitle = getString(R.string.about_title, info.versionName);
+            titleText.setText(getString(R.string.about_title, info.versionName));
         } catch (NameNotFoundException e) {
-            aboutTitle = "Package not found.";
+            titleText.setText(getString(R.string.app_name));
         }
 
-        message.setText(Html.fromHtml((String) getText(R.string.about_text)));
-        message.setAutoLinkMask(SqueezerActivity.RESULT_OK);
-        message.setMovementMethod(ScrollingMovementMethod.getInstance());
-
-        return new AlertDialog.Builder(getActivity())
-                .setTitle(aboutTitle)
-                .setView(message)
-                .create();
+        Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(view);
+        builder.setPositiveButton(R.string.dialog_positive_ok, null);
+        builder.setNegativeButton(R.string.dialog_license, new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which) {
+                new LicenseDialog()
+                        .show(getActivity().getSupportFragmentManager(), "LicenseDialog");
+            }
+        });
+        return builder.create();
     }
 }
