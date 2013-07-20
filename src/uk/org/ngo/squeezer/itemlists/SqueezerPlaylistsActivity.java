ef7760c5f3a0bc698dea17a4a95c2277700da671
Kurt Aaholst <kaaholst@gmail.com>
2013-07-20 23:47:03 +0200

    Update the UI on return from browsing songs of a playlist
    Use startActivity for result to start the child activity, and update the list
    with any changes, in onActivityResult in the parent activity

--- src/uk/org/ngo/squeezer/itemlists/SqueezerPlaylistsActivity.java
+++ src/uk/org/ngo/squeezer/itemlists/SqueezerPlaylistsActivity.java
@@ -33,48 +33,51 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 public class SqueezerPlaylistsActivity extends SqueezerBaseListActivity<SqueezerPlaylist>{
+    public final static int PLAYLIST_SONGS_REQUEST_CODE = 1;
+    public static final String PLAYLIST_RENAMED = "playlist_renamed";
+    public static final String PLAYLIST_DELETED = "playlist_deleted";
+
+    public static final String CURRENT_PLAYLIST = "currentPlaylist";
+    private static final String CURRENT_INDEX = "currentIndex";
+
     private int currentIndex = -1;
-	private SqueezerPlaylist currentPlaylist;
-    private String oldname;
+    private SqueezerPlaylist currentPlaylist;
+    private String oldName;
     public SqueezerPlaylist getCurrentPlaylist() { return currentPlaylist; }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        currentIndex = savedInstanceState.getInt(CURRENT_INDEX);
+        currentPlaylist = savedInstanceState.getParcelable(CURRENT_PLAYLIST);
+    }
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            currentPlaylist = extras.getParcelable("currentPlaylist");
-        }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(CURRENT_INDEX, currentIndex);
+        outState.putParcelable(CURRENT_PLAYLIST, currentPlaylist);
+        super.onSaveInstanceState(outState);
     }
 
     /**
      * Set the playlist to be used as context
-     * @param index
-     * @param playlist
      */
     public void setCurrentPlaylist(int index, SqueezerPlaylist playlist) {
-	    this.currentIndex = index;
+        this.currentIndex = index;
 	    this.currentPlaylist = playlist;
-	    getIntent().putExtra("currentPlaylist", currentPlaylist);
 	}
 
     /**
      * Rename the playlist previously set as context.
-     * @param newname
      */
-    public void playlistRename(String newname) {
+    public void playlistRename(String newName) {
         try {
-            getService().playlistsRename(getCurrentPlaylist(), newname);
-            if (currentIndex != -1) {
-                oldname = currentPlaylist.getName();
-                getCurrentPlaylist().setName(newname);
-                getItemAdapter().notifyDataSetChanged();
-            } else
-                // We don't know which item to update, so we just reorder
-                orderItems();
+            getService().playlistsRename(currentPlaylist, newName);
+            oldName = currentPlaylist.getName();
+            currentPlaylist.setName(newName);
+            getItemAdapter().notifyDataSetChanged();
         } catch (RemoteException e) {
-            Log.e(getTag(), "Error renaming playlist to '"+ newname + "': " + e);
+            Log.e(getTag(), "Error renaming playlist to '"+ newName + "': " + e);
         }
     }
 
@@ -99,6 +102,21 @@ public class SqueezerPlaylistsActivity extends SqueezerBaseListActivity<Squeezer
 	protected void orderPage(int start) throws RemoteException {
 		getService().playlists(start);
 	}
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        Log.d(getTag(), "onActivityResult(" + requestCode  + "," + resultCode + ",'" + data + "')");
+        if (requestCode == PLAYLIST_SONGS_REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data.getBooleanExtra(PLAYLIST_RENAMED, false)) {
+                currentPlaylist = data.getParcelableExtra(CURRENT_PLAYLIST);
+                getItemAdapter().setItem(currentIndex, currentPlaylist);
+                getItemAdapter().notifyDataSetChanged();
+            }
+            if (data.getBooleanExtra(PLAYLIST_DELETED, false)) {
+                orderItems();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -142,8 +160,8 @@ public class SqueezerPlaylistsActivity extends SqueezerBaseListActivity<Squeezer
 
 		@Override
         public void onRenameFailed(String msg) throws RemoteException {
-		    if (currentIndex != -1)
-		        currentPlaylist.setName(oldname);
+            if (currentIndex != -1)
+		        currentPlaylist.setName(oldName);
 			showServiceMessage(msg);
 		}
 
