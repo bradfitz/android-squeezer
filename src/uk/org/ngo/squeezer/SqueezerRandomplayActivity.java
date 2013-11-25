e407805a41d65ad4e22427c0cc5fea03ea78fe78
Nik Clayton <nik@localhost>
2013-11-25 12:58:51 +0100

    Remove empty switch statement.

--- src/uk/org/ngo/squeezer/SqueezerRandomplayActivity.java
+++ src/uk/org/ngo/squeezer/SqueezerRandomplayActivity.java
@@ -71,8 +71,6 @@ public class SqueezerRandomplayActivity extends SqueezerBaseActivity {
 				NowPlayingActivity.show(SqueezerRandomplayActivity.this);
 				return;
 			}
-			switch (position) {
-			}
 		}
 	};
 
