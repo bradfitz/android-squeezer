42f53536a4bcba935d1b959764d4507b1605d3a4
Kurt Aaholst <kaaholst@gmail.com>
2014-04-05 23:02:21 +0200

    Fix player subscription state not updated
    when callback is unregistered

--- Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/service/SqueezeService.java
@@ -1397,6 +1397,7 @@ public class SqueezeService extends Service implements ServiceCallbackList.Servi
                     entry.getValue().unregister(entry.getKey());
                 }
             }
+            updatePlayerSubscriptionState();
         }
 
 
