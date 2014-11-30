728143efded502e77f73219ed79b24e1625f0563
Nik Clayton <nik@ngo.org.uk>
2014-11-30 18:37:33 +0100

    Check for a null getPlayStatus() response.
    Annotate the response as @Nullable, with Javadoc that explains why.

--- Squeezer/src/main/java/uk/org/ngo/squeezer/model/PlayerState.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/model/PlayerState.java
@@ -161,6 +161,12 @@ public class PlayerState implements Parcelable {
         return playStatus == PlayStatus.play;
     }
 
+    /**
+     * @return the player's state. May be null, which indicates that Squeezer has received
+     *     a "players" response for this player, but has not yet received a status message
+     *     for it.
+     */
+    @Nullable
     public PlayStatus getPlayStatus() {
         return playStatus;
     }
