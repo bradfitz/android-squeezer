00afb7b593f9cfc222274a0d3b45f5ac8c32f315
Kurt Aaholst <kaaholst@gmail.com>
2019-11-16 15:10:02 +0100

    Don't use AppCompatDelegate.setCompatVectorFromResourcesEnabled
    1) This feature is disabled per default as per:
    https://developer.android.com/reference/android/support/v7/app/AppCompatDelegate.html#setcompatvectorfromresourcesenabled
    2) Even with this enabled we still crash on API 21 when inflate a drawable
    with a nested vector drawable.
    3) We dont need it anymore as we are now generating nested drawables in
    code (as the gravity property doesn't work on earlier android versions).

--- Squeezer/src/main/java/uk/org/ngo/squeezer/framework/BaseActivity.java
+++ Squeezer/src/main/java/uk/org/ngo/squeezer/framework/BaseActivity.java
@@ -30,7 +30,6 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringDef;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NavUtils;
 import androidx.core.app.TaskStackBuilder;
 import androidx.appcompat.app.ActionBar;
@@ -144,7 +143,6 @@ public abstract class BaseActivity extends AppCompatActivity implements HasUiThr
     @CallSuper
     protected void onCreate(android.os.Bundle savedInstanceState) {
         mTheme.onCreate(this);
-        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
         super.onCreate(savedInstanceState);
 
         // Set the icon as the home button, and display it.
